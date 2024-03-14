17ad7960374d336ee83d957713b217fe96bb8681
Anonymous4045 <github@anon4045.email>
2024-03-13 22:44:12 -0500

    Actually make it runnable

--- src/pyweek37/__init__.py
+++ src/pyweek37/__init__.py
@@ -1,2 +1,7 @@
 def hello() -> str:
     return "Hello from pyweek37!"
+
+
+def main():
+    from .__main__ import main
+    main()
--- src/pyweek37/__main__.py
+++ src/pyweek37/__main__.py
@@ -218,5 +218,4 @@ def main():
     arcade.run()
 
 
-if __name__ == "__main__":
-    main()
+main()

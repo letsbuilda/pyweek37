b0954c7e87a65454587250f8b185c0abcd3fa2a2
Anonymous4045 <github@anon4045.email>
2024-03-18 21:52:20 -0500

    Split into files

---
+++ run_game.py
@@ -0,0 +1,4 @@
+from src.pyweek37.__main__ import main
+
+if __name__ == "__main__":
+    main()
---
+++ src/pyweek37/sprites/player.py
@@ -0,0 +1,15 @@
+import arcade
+
+from ..constants import SPRITE_SCALING_PLAYER
+
+
+class PlayerSprite(arcade.Sprite):
+    """ Player Sprite """
+    def __init__(self):
+        """ Init """
+        super().__init__()
+
+        self.scale = SPRITE_SCALING_PLAYER
+
+    def pymunk_moved(self, physics_engine, dx, dy, d_angle):
+        """ Handle being moved by the pymunk engine """
